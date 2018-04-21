1ac274a1e76f87fa174ca9d010bebad207be8bf5
pr0jectf3 <portillozachary@gmail.com>
2018-04-20 18:54:49 -0700

    finished

--- homework2/cookie-clicker-hw2-5/src/main/java/edu/csula/web/AuthenticationServlet.java
+++ homework2/cookie-clicker-hw2-5/src/main/java/edu/csula/web/AuthenticationServlet.java
@@ -1,5 +1,8 @@
 package edu.csula.web;
 
+import edu.csula.storage.UsersDAO;
+import edu.csula.storage.servlet.UsersDAOImpl;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Collection;
@@ -17,16 +20,39 @@ public class AuthenticationServlet extends HttpServlet {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		// TODO: render the authentication page HTML
-		out.println("<h1>Hello login servlet!</h1>");
+        doDelete(request, response);
+		String html = "<h1>User Login</h1>";
+        html +=  "<link rel='stylesheet' type='text/css' href='" + request.getContextPath() + "/app.css' />";
+		html += "<form method = 'POST'>";
+        html += "Username:";
+        html += "<input name = 'username' type = \"text\" /><br/><br/>";
+        html += "</br/>\n" +
+                "      Password:";
+        html += "<input name = 'password'type = \"password\" />\n" +
+                "    </br/>\n" +
+                "    <br/>";
+        html += "<button>Login</button>";
+        html += "</form>";
+        html += "";
+
+		out.println(html);
 	}
 
 	@Override
 	public void doPost( HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO: handle login
+        UsersDAO dao = new UsersDAOImpl(request.getSession());
+        if(dao.authenticate(request.getParameter("username"), request.getParameter("password"))){
+            response.sendRedirect("generators");
+        } else {
+            response.sendRedirect("auth");
+        }
 	}
 
     @Override
     public void doDelete( HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO: handle logout
+        UsersDAO dao = new UsersDAOImpl(request.getSession());
+        dao.logout();
     }
 }
